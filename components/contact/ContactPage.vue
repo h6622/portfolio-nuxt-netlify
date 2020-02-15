3312f596bcd7e8487d4e2946493b6432a98a0924
h662 <godh662@gmail.com>
2020-02-15 12:14:13 +0900

    setting pages

---
+++ components/contact/ContactPage.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    Contact Page
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+.container {
+  /* 임시값 */
+  width: 100%;
+  height: 25vh;
+  background-color: yellow;
+}
+</style>
---
+++ components/main/MainPage.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    Main Page
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+.container {
+  /* 임시값 */
+  width: 100%;
+  height: 25vh;
+  background-color: red;
+}
+</style>
